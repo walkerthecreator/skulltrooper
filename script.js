1ad128646221c215f798060da983f251ac30ac48
walkerthecreator <nitinjaswal2616@gmail.com>
2024-01-02 11:47:06 +0530

    adding html

---
+++ script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily : {
+            inter : ['Inter' , 'sans-serif'] ,
+            poppins : ['Poppins' , 'sans-serif']
+        } ,
+        colors: {
+          clifford: '#da373d',
+        } ,
+        
+      }
+    }
+  }
